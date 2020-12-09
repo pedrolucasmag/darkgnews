bf832fb8c6be76d39cb5b17b00a6ec1d2be4dc64
Pedro Lucas Oliveira <6821539+pedrolucasmag@users.noreply.github.com>
2020-12-09 13:00:16 -0300

    fixed search bar and header colors

--- background.js
+++ background.js
@@ -2,11 +2,11 @@ chrome.runtime.onInstalled.addListener(details => {
   switch (details.reason) {
     case 'install':
       chrome.tabs.create({url: 'https://pedrolucasmag.github.io/dark-themes/gnews'});
-	    console.log("New tab launched with https://pedrolucasmag.github.io/dark-themes/gnews");
+	  console.log("New tab launched with https://pedrolucasmag.github.io/dark-themes/gnews");
       break;
 
     case 'update':
-      	console.log ("Dark theme for Gnews Updated!");
+	  console.log("Dark Theme for Google News Updated!");
       break;
   }
 });
